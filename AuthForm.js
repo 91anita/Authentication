affda558f615f7ef458a5915bbb8d1293a4bed81
91anita <45237162+91anita@users.noreply.github.com>
2023-04-22 01:16:28 +0530

    Updating the Old Password

--- AuthForm.js
+++ AuthForm.js
@@ -1,9 +1,12 @@
-import { useState, useRef, useContext } from "react";
+import { useState, useRef, useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
+import AuthContext from '../../store/auth-context';
 
-import AuthContext from "../../store/auth-context";
-import classes from "./AuthForm.module.css";
+import classes from './AuthForm.module.css';
 
 const AuthForm = () => {
+
+  const navigate = useNavigate();
   const emailInputRef = useRef();
   const passwordInputRef = useRef();
 
@@ -61,6 +64,7 @@ const AuthForm = () => {
       })
       .then((data) => {
         authCtx.login(data.idToken);
+        navigate("/")
       })
       .catch((err) => {
         alert(err.message);
